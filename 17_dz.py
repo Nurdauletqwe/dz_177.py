8eeb81bade9d7e382543e7bcc097010d7cb5e1ee
Nurdaulet <134767660+Nurdauletqwe@users.noreply.github.com>
2024-04-04 22:40:55 +0500

    Add files via upload

---
+++ 17_dz.py
@@ -0,0 +1,26 @@
+# class First:
+#     def __init__(self, text):
+#         self.text = text
+
+#     def set_text(self, text=None):
+#         if text:
+#             self.text = text
+#         else:
+#             self.text = input('Введите текст: ')
+
+#         def display_text(self):
+#             print(self.text)
+
+
+# class Second(First):
+#     def __init__(self, text):
+#         super().__init__(text)
+
+#     def display_text(self):
+#         print('Текст:', self.text)
+
+
+# first = First('Hello')
+# second = Second('World')
+# first.set_text()
+# second.display_text()
